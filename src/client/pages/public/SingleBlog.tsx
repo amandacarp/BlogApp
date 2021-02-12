3326fde47935c8f93398e8f1e244b94bf5c1a7f4
Amanda Carpentieri <carpentieri.a@gmail.com>
2021-02-11 16:16:18 -0800

    comment functionality

--- src/client/pages/public/SingleBlog.tsx
+++ src/client/pages/public/SingleBlog.tsx
@@ -7,7 +7,6 @@ import apiService from '../../utils/api-service';
 
 const SingleBlog = (props: singleBlogProps) => {
     const { id } = useParams<{ id: string }>()
-    const {blogid} = useParams<{blogid: string}>()
     const [blog, setSingleBlog] = useState<IBlog>(null);
     const [blogTag, setSingleBlogTag] = useState<BlogTags[]>(null);
     const [comments, setComments] = useState<Comments[]>(null)
@@ -31,26 +30,34 @@ const SingleBlog = (props: singleBlogProps) => {
             .then(blogTag => setSingleBlogTag(blogTag));
     }, [id])
 
+    const handleAddComment = async (e:React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        await apiService('/api/comments', 'POST', {content, blogid: id});
+        const comments = await apiService(`/api/comments/blogs/${id}`);
+        setComments(comments);
+        setContent('');
+    }
+
 
     return (
         <>
         <BlogCard key={blog?.id} blog={blog} blogTag={blogTag} />
 
-        <section className="row justify-content-center mt-3">
+        <section className="row justify-content-center mt-3 ml-3 mr-3">
 				<div className="col-md-5 order-2 order-md-1">
-					<form className="form-group bg-white border p-3">
-						<label htmlFor="comment content">Comment</label>
+					<form className="form-group bg-white border p-3 mb-3">
+						<label id="labelSelect" htmlFor="comment content">Post your Comment Here!</label>
 						<textarea value={content} onChange={e => setContent(e.target.value)} rows={5} className="form-control"/>
-						<button className="btn btn-primary mt-2">Add Comment</button>
+						<button id="buttonSingle" onClick={handleAddComment} className="btn mt-2">Add Comment</button>
 					</form>
 				</div>
-				<div className="col-md-7 order-1 order-md-2">
+				<div className="col-md-7 order-1 order-md-2 mb-3">
 					<ul className="list-group">
-                    <label htmlFor="comment content">Comments</label>
+                    <label id="label" htmlFor="comment content">Comments</label>
 						{comments?.map(comment => (
 							<li key={comment.id} className="list-group-item">
-								<small className="font-weight-bold d-block mb-2">{comment?.first_name} {comment?.last_name}</small>
-								<p>{comment?.content}</p>
+								<small id="labelSelect" className="font-weight-bold d-block mb-2">{comment?.first_name} {comment?.last_name}</small>
+								<p id="labelSelect">{comment?.content}</p>
 							</li>
 						))}
 					</ul>
